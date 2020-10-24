3533e522c7b7ad41bab52464816e0d3f0758b307
Gleb Chipiga <gleb.chipiga+git@pm.me>
2020-10-24 20:09:01 +0300

    Remove type ignore comment from aiosqlite module

--- aiotgbot/storage_sqlite.py
+++ aiotgbot/storage_sqlite.py
@@ -2,7 +2,7 @@ import asyncio
 import json
 from typing import Dict, List, Optional, Tuple, Union
 
-import aiosqlite  # type: ignore
+import aiosqlite
 
 from .storage import BaseStorage
 from .utils import json_dumps
