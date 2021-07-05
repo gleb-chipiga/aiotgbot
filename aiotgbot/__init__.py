2b8a889a3642ae36f1e5ab7659dc54ff6e62a95d
Gleb Chipiga <gleb.chipiga+git@pm.me>
2021-07-05 21:24:13 +0300

    Version 0.10.8

--- aiotgbot/__init__.py
+++ aiotgbot/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.10.7'
+__version__ = '0.10.8'
 
 from .api_types import (BaseTelegram, CallbackQuery, Chat, ChosenInlineResult,
                         Contact, File, InlineKeyboardButton,
